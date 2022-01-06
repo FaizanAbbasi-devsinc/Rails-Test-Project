fcb29ce458631e4e548c4a65ccf96f26fe418273
abbasifaizan70 <abbasi.faizan1997@gmail.com>
2022-01-06 19:41:02 +0500

    rubocop error solve

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
-# frozen_string_literal: true
-
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  validates :name, presence: true, format: { with: /\A[^0-9`!@#\$%\^&*+_=]+\z/, message: 'only allows letters' }
+  validates :name, presence: true, format: { with: /\A[^0-9`!@#$%\^&*+_=]+\z/, message: 'only allows letters' }
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
