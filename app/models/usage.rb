b1a3e66b8f8fde8212b1e6e01ed7d314750d4695
abbasifaizan70 <abbasi.faizan1997@gmail.com>
2022-01-13 16:45:34 +0500

    add stripe service

--- app/models/usage.rb
+++ app/models/usage.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class Usage < ApplicationRecord
-  validates :used_unit, presence: true, numericality: { greater_than_or_equal_to: 1 }
+  validates :used_unit, presence: true  
   belongs_to :subscription
 end
